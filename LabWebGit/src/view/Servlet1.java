7cbe1ddfffa265606839c6e86a488d921af8d03c
黃建瑋 <chad51003@yahoo.com.tw>
2017-07-24 00:20:45 +0800

    第一次commit 將.java 、 .xml、.jar、.jsp檔以及igonre檔加入Git Repository

---
+++ LabWebGit/src/view/Servlet1.java
@@ -0,0 +1,33 @@
+package view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		long time = System.currentTimeMillis();
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Servlet1:"+time+"</h3>");
+		out.close();
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		this.doGet(request, response);
+	}
+
+}
